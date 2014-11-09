d64193c3763b5993587f4c2af9aee243cbe5c2f6
zkocen <zkocen@gmail.com>
2014-11-09 19:32:21 +0000

    invalidateSession

---
+++ src/com/worldmanager/servlets/InvalidateSessionAndRemoveCookies.java
@@ -0,0 +1,54 @@
+package com.worldmanager.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class InvalidateSessionAndRemoveCookies
+ */
+@WebServlet("/InvalidateSessionAndRemoveCookies")
+public class InvalidateSessionAndRemoveCookies extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InvalidateSessionAndRemoveCookies() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession s = request.getSession();
+		s.invalidate();
+		
+		Cookie[] MyCookies = request.getCookies();
+		if (MyCookies != null && MyCookies.length > 0) {
+			for (Cookie c : MyCookies) {
+				if (c.getName().toLowerCase().contains("credentials")) {
+					c.setMaxAge(0);
+					response.addCookie(c);
+				}
+			}
+		}
+		response.sendRedirect("ClearSessionAndCookies.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
